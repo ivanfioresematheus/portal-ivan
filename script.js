757d2a88359c121c3d3e16a2db7701dd1ac30872
ivanfioresematheus <fioreseivanmatheus@gmail.com>
2026-07-21 18:06:15 -0300

    atualiza script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-const jogador = {
-  nome: "Ivan",
-  idade: 17,
-  clube: "Botafogo-SP",
-  posicao: "Meia-atacante",
-  camisa: 8,
-  overall: 69,
+const portal = {
+  jogador: {
+    nome: "Ivan",
+    idade: 17,
+    clube: "Botafogo-SP",
+    camisa: 8,
+    posicao: "Meia-atacante",
+    overall: 69,
+    jogos: 10,
+    gols: 7,
+    assistencias: 0,
+    participacoes: 7,
+    valorMercado: "US$ 800.000"
+  },
 
-  jogos: 10,
-  gols: 7,
-  assistencias: 0,
-  participacoes: 7,
+  ultimoJogo: {
+    casa: "Botafogo-SP",
+    golsCasa: 1,
+    visitante: "Ponte Preta",
+    golsVisitante: 1
+  },
 
-  valorMercado: "US$ 800 mil",
-
-  ultimoJogo: "Botafogo-SP 1 x 1 Ponte Preta",
-  proximoJogo: "Ceará x Botafogo-SP"
+  proximoJogo: {
+    casa: "Ceará",
+    visitante: "Botafogo-SP"
+  }
 };
 
 function atualizarHome() {
@@ -23,10 +32,22 @@ function atualizarHome() {
   const assistencias = document.getElementById("assistencias");
   const valor = document.getElementById("valor");
 
-  if (jogos) jogos.textContent = jogador.jogos;
-  if (gols) gols.textContent = jogador.gols;
-  if (assistencias) assistencias.textContent = jogador.assistencias;
-  if (valor) valor.textContent = jogador.valorMercado;
+  if (jogos) jogos.textContent = portal.jogador.jogos;
+  if (gols) gols.textContent = portal.jogador.gols;
+  if (assistencias) assistencias.textContent = portal.jogador.assistencias;
+  if (valor) valor.textContent = portal.jogador.valorMercado;
+
+  const ultimo = document.getElementById("ultimoJogo");
+  if (ultimo) {
+    ultimo.textContent =
+      `${portal.ultimoJogo.casa} ${portal.ultimoJogo.golsCasa} x ${portal.ultimoJogo.golsVisitante} ${portal.ultimoJogo.visitante}`;
+  }
+
+  const proximo = document.getElementById("proximoJogo");
+  if (proximo) {
+    proximo.textContent =
+      `${portal.proximoJogo.casa} x ${portal.proximoJogo.visitante}`;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", atualizarHome);
