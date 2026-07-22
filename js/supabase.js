2469cc76d14f55649b6173714fcb8564b7837787
ivanfioresematheus <fioreseivanmatheus@gmail.com>
2026-07-22 14:52:26 -0300

    Update supabase.js
    Cria conexão Supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
-// Em construção
+const SUPABASE_URL = "https://ywjkuskimdtxymathnxj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_VNSgRxU9GBi-XVY_iPZl8g_HoG-UGuz";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
