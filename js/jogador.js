cc0636fad3534e58fce38c20874d15f7981294a3
ivanfioresematheus <fioreseivanmatheus@gmail.com>
2026-07-22 14:53:51 -0300

    Update jogador.js

--- js/jogador.js
+++ js/jogador.js
@@ -1,1 +1,28 @@
-// Em construção
+async function carregarJogador() {
+
+  const { data, error } = await supabaseClient
+    .from("jogador")
+    .select("*")
+    .eq("id", 1)
+    .single();
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const jogos = document.getElementById("jogos");
+  const gols = document.getElementById("gols");
+  const assistencias = document.getElementById("assistencias");
+  const valor = document.getElementById("valor");
+
+  if (jogos) jogos.textContent = data.jogos;
+  if (gols) gols.textContent = data.gols;
+  if (assistencias) assistencias.textContent = data.assistencias;
+
+  if (valor) {
+    valor.textContent =
+      "US$ " + Number(data.valor_mercado).toLocaleString("pt-BR");
+  }
+
+}
